1d70930b069a7a863c2f982e968278af852af603
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-26 16:53:14 +1000

    Search bar done

--- employee-directory/src/components/SearchBar.js
+++ employee-directory/src/components/SearchBar.js
@@ -3,9 +3,14 @@ import React from "react";
 
 function SearchBar() {
   return (
-    <nav  className="SearchBar">
-      <p >Welcome</p>
-    </nav>
+    <form className="d-flex justify-content-center form-inline">
+      <input 
+        className="form-control mr-sm-2" 
+        type="search" 
+        placeholder="Search" 
+        aria-label="Search">
+      </input>
+    </form>
   );
 }
 
