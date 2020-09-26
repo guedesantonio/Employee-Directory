2935ea969ce7be1680916b7782dea4431c2d1d38
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-26 16:14:04 +1000

    App.js components imported

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
-import Section from "./components/SearchBar";
-import Navbar from "./components/EmployeeList";
+import SearchBar from "./components/SearchBar";
+import EmployeeList from "./components/EmployeeList";
 function App() {
   return (
-    <Header/>
-    <SearchBar/>
-    <EmployeeList/>
+    <Homepage>
+      <Header/>
+      <SearchBar/>
+      <EmployeeList/>
+    </Homepage>
 
   );
 }
--- employee-directory/src/components/EmployeeList.js
+++ employee-directory/src/components/EmployeeList.js
@@ -1,24 +1,9 @@
 import React from "react";
-import "../styles/Section.css";
 
-// By importing the Section.css file, it is added to the DOM whenever this component loads
 
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
-
-const styles = {
-  sectionStyles: {
-    background: "orange"
-  }
-};
-
-// We use JSX curly braces to evaluate the style object on the JSX tag
-
-function Section() {
+function EmployeeList() {
   return (
-    <section style={styles.sectionStyles} className="section">
+    <section>
       <h2>Lorem Ipsum Dolor Sit Amet</h2>
       <p>
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
@@ -55,4 +40,4 @@ function Section() {
   );
 }
 
-export default Section;
+export default EmployeeList;
