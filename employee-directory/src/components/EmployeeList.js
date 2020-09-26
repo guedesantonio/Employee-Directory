e496d7a37ad167e4d76f7baae540604e4c71f235
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-26 17:46:51 +1000

    employee list header done

--- employee-directory/src/components/EmployeeList.js
+++ employee-directory/src/components/EmployeeList.js
@@ -3,12 +3,14 @@ import './EmployeeList.css';
 
 function EmployeeList() {
   return (
-    <div className="d-flex justify-content-center row">
-      <div className="col">Image</div>
-      <div className="col">Name</div>
-      <div className="col">Phone</div>
-      <div className="col">Email</div>
-      <div className="col">DOB</div>
+    <div className="container-fluid text-center">
+      <div className="row">
+        <div className="col">Image</div>
+        <div className="col">Name</div>
+        <div className="col">Phone</div>
+        <div className="col">Email</div>
+        <div className="col">DOB</div>
+      </div>
     </div>
   );
 }
