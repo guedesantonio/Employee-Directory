fddd3aec2483acf9d81fb30097a08a624ba18512
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-27 20:43:53 +1000

    Search form back to app.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Wrapper from "./components/Wrapper/Wrapper.js";
 import Header from "./components/Header/Header";
-import SearchBar from "./components/SearchBar/SearchBar";
 import EmployeeList from "./components/EmployeeList/EmployeeList";
 import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
 import employees from "./employees.json"
@@ -22,7 +21,14 @@ class App extends Component {
     return (
       <Wrapper>
           <Header/>
-          <SearchBar/>
+          <form className="d-flex justify-content-center form-inline">
+          <input 
+            className="form-control mr-sm-2" 
+            type="search" 
+            placeholder="Search" 
+            aria-label="Search">
+          </input>
+          </form>
           <div className="container-fluid text-center">
             <EmployeeList/>
             {this.state.employees.map(employee=> (
